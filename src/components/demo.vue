572873e947b591b46d9e14852c15f04b1a4b64cd
sco <sco@didichuxing.com>
2018-07-18 21:41:34 +0800

    coding

---
+++ src/components/demo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="demo">
+      <p>这是子组件</p>
+      <p>{{msg}}</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'demo.vue',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+  .demo{
+    width: 400px;
+    margin: 0 auto;
+    margin-top: 50px;
+    border: 1px solid red;
+  }
+  p {
+    line-height: 50px;
+  }
+</style>
